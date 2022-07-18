8ba9aac30d308af7cc86103ef79ff6db2fc5018c
lasinduwathsan <109015894+lasinduwathsan@users.noreply.github.com>
2022-07-19 00:03:04 +0530

    Add files via upload

---
+++ Q2.scala
@@ -0,0 +1,14 @@
+object Q2 {
+  def main(args: Array[String]): Unit ={
+    val celcieus:Double=35
+    val Fharenheit:Double=celcieus*1.8000+32.00
+    println("the value is :"+Fharenheit)
+
+
+
+
+
+  }
+
+
+}
